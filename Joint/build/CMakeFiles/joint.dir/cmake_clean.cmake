72c55301e425c87b4518a9ffef56af41383eb2d7
ClaudioMuselli <claudio.muselli@mi.infn.it>
2017-01-31 15:58:29 +0100

    Joint kdevelop

---
+++ Joint/build/CMakeFiles/joint.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/joint.dir/main.cpp.o"
+  "joint.pdb"
+  "joint"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/joint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
